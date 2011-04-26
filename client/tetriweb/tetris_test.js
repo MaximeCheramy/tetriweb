0a360b75a304e504ba11542dab03370fea7cc2e4
MaximeCheramy <maxime81@gmail.com>
2011-04-26 20:38:19 +0000

    Test connect. refs #754

--- client/tetriweb/tetris_test.js
+++ client/tetriweb/tetris_test.js
@@ -1,14 +1,32 @@
 goog.require('goog.testing.jsunit');
+goog.require('goog.testing.net.XhrIo');
+goog.require('goog.testing.recordConstructor');
 goog.require('tetriweb.Graphics');
 goog.require('tetriweb.Tetrinet');
 goog.require('tetriweb.Tetris');
 
 function setUp() {
+  goog.net.XhrIo = goog.testing.recordConstructor(goog.testing.net.XhrIo);
 }
 
 function testConnect() {
   var tetrinet = new tetriweb.Tetrinet();
 
-  // TODO: overrider ce qu'il faut pour éviter de devoir communiquer avec le serveur.
   tetrinet.connect('test', 'teamtest');
+  
+  // Connection succeed.
+  var xhr = goog.net.XhrIo.getLastCall().getThis();
+  xhr.simulateResponse(200, '{"pnum":1}');
+
+  // Check playerExists.
+  assertTrue(tetrinet.playerExists(1));
+  assertFalse(tetrinet.playerExists(0));
+  assertFalse(tetrinet.playerExists(2));
+
+  // Hide the connect form and display the chat-area.
+  assertTrue(goog.dom.classes.has(goog.dom.getElement('connect-form'), 'hid'));
+  assertFalse(goog.dom.classes.has(goog.dom.getElement('chat-area'), 'hid'));
+
+  // PlayerList must contains only one connected user.
+  assertEquals(goog.dom.getElement('player-list').children.length, 1);
 }
