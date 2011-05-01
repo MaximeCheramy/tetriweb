496e18fb68fd1fdd63592f89438cc59ae4b5e2a6
MaximeCheramy <maxime81@gmail.com>
2011-05-01 16:01:47 +0000

    Fonctions par ordre alphabetique (sauf le init en premier). refs #711

--- client/tetriweb/graphics.js
+++ client/tetriweb/graphics.js
@@ -30,49 +30,10 @@ tetriweb.Graphics.domInit = function(pnum, nickname) {
 
 
 /**
- * Preloads the images used by the UI.
+ * Clears the event log.
  */
-tetriweb.Graphics.preloadImages = function() {
-  var theme = new Image();
-  theme.src = 'img/theme.png';
-};
-
-
-/**
- * Enable/disable moderator controls
- * @param {boolean} enable Whether enable or disable the controls.
- */
-tetriweb.Graphics.enableModeratorControls = function(enable) {
-  goog.dom.getElement('start-game').disabled = !enable;
-  goog.dom.getElement('stop-game').disabled = !enable;
-};
-
-
-/**
- * Disables the player's field.
- */
-tetriweb.Graphics.disableField = function() {
-  var mask = goog.dom.createDom('div', {id: 'field-mask'});
-  goog.dom.appendChild(goog.dom.getElement('my-field'), mask);
-};
-
-
-/**
- * Enables the player's field.
- */
-tetriweb.Graphics.enableField = function() {
-  goog.dom.removeNode(goog.dom.getElement('field-mask'));
-};
-
-
-/**
- * Shows the fields and hides the chat area.
- */
-tetriweb.Graphics.displayFields = function() {
-  goog.dom.classes.remove(goog.dom.getElement('fields'), 'hid');
-  goog.dom.classes.add(goog.dom.getElement('chat-area'), 'hid');
-
-  tetriweb.Graphics.domWriteMessage = tetriweb.Graphics.domLogEvent;
+tetriweb.Graphics.clearEventLog = function() {
+  goog.dom.removeChildren(tetriweb.Graphics.eventLog);
 };
 
 
@@ -98,6 +59,26 @@ tetriweb.Graphics.displayLoginForm = function() {
 
 
 /**
+ * Disables the player's field.
+ */
+tetriweb.Graphics.disableField = function() {
+  var mask = goog.dom.createDom('div', {id: 'field-mask'});
+  goog.dom.appendChild(goog.dom.getElement('my-field'), mask);
+};
+
+
+/**
+ * Shows the fields and hides the chat area.
+ */
+tetriweb.Graphics.displayFields = function() {
+  goog.dom.classes.remove(goog.dom.getElement('fields'), 'hid');
+  goog.dom.classes.add(goog.dom.getElement('chat-area'), 'hid');
+
+  tetriweb.Graphics.domWriteMessage = tetriweb.Graphics.domLogEvent;
+};
+
+
+/**
  * Displays pause.
  */
 tetriweb.Graphics.displayPause = function() {
@@ -107,125 +88,6 @@ tetriweb.Graphics.displayPause = function() {
   goog.dom.setTextContent(pause, 'Pause');
   goog.dom.appendChild(fieldMask, pause);
 };
-
-
-/**
- * Hides pause.
- */
-tetriweb.Graphics.hidePause = function() {
-  tetriweb.Graphics.enableField();
-};
-
-
-/**
- * Sets the error message on the login form.
- * @param {string} message The message to set.
- */
-tetriweb.Graphics.setErrorMessage = function(message) {
-  var errorMessage = goog.dom.getElement('error-message');
-  goog.dom.setTextContent(errorMessage, message);
-  goog.dom.classes.remove(errorMessage, 'hid');
-};
-
-
-/**
- * Gives the focus to the event message input box.
- */
-tetriweb.Graphics.eventMsgFocus = function() {
-  goog.dom.getElement('event-msg').focus();
-};
-
-
-/**
- * Gives the focus to the game area.
- */
-tetriweb.Graphics.gameAreaFocus = function() {
-  goog.dom.getElement('my-field').focus();
-};
-
-
-/**
- * Handles the focus when a new game is started.
- */
-tetriweb.Graphics.newGameFocus = function() {
-  var partyMsg = goog.dom.getElement('party-msg');
-  var eventMsg = goog.dom.getElement('event-msg');
-  if (document.activeElement == partyMsg) {
-    tetriweb.Graphics.eventMsgFocus();   
-    eventMsg.value = partyMsg.value;
-    partyMsg.value = '';
-  } else {
-    tetriweb.Graphics.gameAreaFocus();
-  }
-};
-
-
-/**
- * Clears the event log.
- */
-tetriweb.Graphics.clearEventLog = function() {
-  goog.dom.removeChildren(tetriweb.Graphics.eventLog);
-};
-
-
-/**
- * Displays an event in the event window.
- * @param {string} message The message to display.
- */
-tetriweb.Graphics.domLogEvent = function(message) {
-  var eventLog = tetriweb.Graphics.eventLog;
-  var cont = goog.dom.createDom('div');
-  cont.innerHTML = message;
-  goog.dom.appendChild(eventLog, cont);
-  eventLog.scrollTop = eventLog.scrollHeight; // scroll to bottom
-};
-
-
-/**
- * Escapes HTML special characters in a string.
- * @param {string} str The string to escape.
- * @return {string} The escaped string.
- */
-tetriweb.Graphics.htmlspecialchars = function(str) {
-   str = str.replace(/&/g, '&amp;');
-   str = str.replace(/\"/g, '&quot;');
-   str = str.replace(/\'/g, '&#039;');
-   str = str.replace(/</g, '&lt;');
-   str = str.replace(/>/g, '&gt;');
-   return str;
-};
-
-
-/**
- * Highlights a nickname in a message.
- * @param {string} message The message.
- * @param {string} nick The nickname to highlight.
- * @return {string} The message, in which the nickname has been highlighted.
- */
-tetriweb.Graphics.hlNick = function(message, nick) {
-  var reg = new RegExp('\\b' + nick + '\\b', 'g');
-  message = tetriweb.Graphics.htmlspecialchars(message);
-  return message.replace(reg, '<span class="hl">' + nick + '</span>');
-};
-
-
-/**
- * Writes a message on the partyline DOM element.
- * @param {string} msg The message to write.
- */
-tetriweb.Graphics.domWritePline = function(msg) {
-  var pline = goog.dom.getElement('partyline');
-  var cont = goog.dom.createDom('div');
-  cont.innerHTML = msg;
-  goog.dom.appendChild(pline, cont);
-  pline.scrollTop = pline.scrollHeight; // scroll to bottom
-};
-
-
-/**
- * Function to display a message to the user (pline or log)
- */
-tetriweb.Graphics.domWriteMessage = tetriweb.Graphics.domWritePline;
 
 
 /**
@@ -271,6 +133,81 @@ tetriweb.Graphics.domInitField = function(player_id, nickname) {
 
 
 /**
+ * Enables the player's field.
+ */
+tetriweb.Graphics.enableField = function() {
+  goog.dom.removeNode(goog.dom.getElement('field-mask'));
+};
+
+
+/**
+ * Enable/disable moderator controls
+ * @param {boolean} enable Whether enable or disable the controls.
+ */
+tetriweb.Graphics.enableModeratorControls = function(enable) {
+  goog.dom.getElement('start-game').disabled = !enable;
+  goog.dom.getElement('stop-game').disabled = !enable;
+};
+
+
+/**
+ * Gives the focus to the event message input box.
+ */
+tetriweb.Graphics.eventMsgFocus = function() {
+  goog.dom.getElement('event-msg').focus();
+};
+
+
+/**
+ * Gives the focus to the game area.
+ */
+tetriweb.Graphics.gameAreaFocus = function() {
+  goog.dom.getElement('my-field').focus();
+};
+
+
+/**
+ * Hides pause.
+ */
+tetriweb.Graphics.hidePause = function() {
+  tetriweb.Graphics.enableField();
+};
+
+
+/**
+ * Displays an event in the event window.
+ * @param {string} message The message to display.
+ */
+tetriweb.Graphics.domLogEvent = function(message) {
+  var eventLog = tetriweb.Graphics.eventLog;
+  var cont = goog.dom.createDom('div');
+  cont.innerHTML = message;
+  goog.dom.appendChild(eventLog, cont);
+  eventLog.scrollTop = eventLog.scrollHeight; // scroll to bottom
+};
+
+
+/**
+ * Writes a message on the partyline DOM element.
+ * @param {string} msg The message to write.
+ */
+tetriweb.Graphics.domWritePline = function(msg) {
+  var pline = goog.dom.getElement('partyline');
+  var cont = goog.dom.createDom('div');
+  cont.innerHTML = msg;
+  goog.dom.appendChild(pline, cont);
+  pline.scrollTop = pline.scrollHeight; // scroll to bottom
+};
+
+
+/**
+ * Function to display a message to the user (pline or log)
+ */
+tetriweb.Graphics.domWriteMessage = tetriweb.Graphics.domWritePline;
+
+
+
+/**
  * Removes a field from the DOM.
  * @param {number} player_id The field owner.
  */
@@ -289,6 +226,66 @@ tetriweb.Graphics.domDestroyField = function(player_id) {
 tetriweb.Graphics.domSetBlock = function(player_id, x, y, type) {
   var block = goog.dom.getElement('block-' + player_id + '-' + y + '-' + x);
   goog.dom.classes.set(block, 'small block ' + tetriweb.Tetris.convert(type));
+};
+
+
+/**
+ * Empties the game field.
+ */
+tetriweb.Graphics.emptyField = function() {
+  goog.dom.removeChildren(tetriweb.Graphics.myField_);
+};
+
+
+/**
+ * Hides the pause and resume buttons.
+ * @param {boolean} enable Whether enable or disable the buttons.
+ */
+tetriweb.Graphics.enablePauseResumeButtons = function(enable) {
+  var pauseButton = goog.dom.getElement('pause-game');
+  var resumeButton = goog.dom.getElement('resume-game');
+
+  pauseButton.disabled = !enable;
+  resumeButton.disabled = !enable;
+};
+
+
+/**
+ * Hides the login form.
+ */
+tetriweb.Graphics.hideLoginForm = function() {
+  var form = goog.dom.getElement('connect-form');
+  var errorMessage = goog.dom.getElement('error-message');
+  goog.dom.classes.add(form, 'hid');
+  goog.dom.classes.add(errorMessage, 'hid');
+};
+
+
+/**
+ * Highlights a nickname in a message.
+ * @param {string} message The message.
+ * @param {string} nick The nickname to highlight.
+ * @return {string} The message, in which the nickname has been highlighted.
+ */
+tetriweb.Graphics.hlNick = function(message, nick) {
+  var reg = new RegExp('\\b' + nick + '\\b', 'g');
+  message = tetriweb.Graphics.htmlspecialchars(message);
+  return message.replace(reg, '<span class="hl">' + nick + '</span>');
+};
+
+
+/**
+ * Escapes HTML special characters in a string.
+ * @param {string} str The string to escape.
+ * @return {string} The escaped string.
+ */
+tetriweb.Graphics.htmlspecialchars = function(str) {
+   str = str.replace(/&/g, '&amp;');
+   str = str.replace(/\"/g, '&quot;');
+   str = str.replace(/\'/g, '&#039;');
+   str = str.replace(/</g, '&lt;');
+   str = str.replace(/>/g, '&gt;');
+   return str;
 };
 
 
@@ -319,51 +316,47 @@ tetriweb.Graphics.layDownPiece = function(curX, curY, current, currentColor) {
 
 
 /**
- * Updates the special bar displayed under the game field.
- * @param {Array.<number>} specialsQueue The specials queue to display.
+ * Moves the current piece to the given horizontal position.
+ * @param {number} posX Horizontal coordinate.
  */
-tetriweb.Graphics.updateSpecialBar = function(specialsQueue) {
-  var convert = tetriweb.Tetris.convert;
+tetriweb.Graphics.moveCurPieceH = function(posX) {
+  tetriweb.Graphics.currentObj_.style.left =
+      posX * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
+};
 
-  // Clear the bar...
-  goog.dom.removeChildren(tetriweb.Graphics.specialBar_);
-  // And fill it again !
-  for (var i = 0; i < specialsQueue.length; i++) {
-    var special = goog.dom.createDom('div');
-    goog.dom.classes.set(special, 'block ' + convert(specialsQueue[i]));
-    special.style.top = 0 + 'px';
-    special.style.left = i * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
-    goog.dom.appendChild(tetriweb.Graphics.specialBar_, special);
+
+/**
+ * Moves the current piece to the given vertical position.
+ * @param {number} posY Vertical coordinate.
+ */
+tetriweb.Graphics.moveCurPieceV = function(posY) {
+  tetriweb.Graphics.currentObj_.style.top =
+      posY * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
+};
+
+
+/**
+ * Handles the focus when a new game is started.
+ */
+tetriweb.Graphics.newGameFocus = function() {
+  var partyMsg = goog.dom.getElement('party-msg');
+  var eventMsg = goog.dom.getElement('event-msg');
+  if (document.activeElement == partyMsg) {
+    tetriweb.Graphics.eventMsgFocus();
+    eventMsg.value = partyMsg.value;
+    partyMsg.value = '';
+  } else {
+    tetriweb.Graphics.gameAreaFocus();
   }
 };
 
 
 /**
- * Updates the player list.
- * @param {Array.<string>} players The player list.
- * @param {Array.<string>} teams The team list.
- * @param {number} moderator The moderator's playernum.
+ * Preloads the images used by the UI.
  */
-tetriweb.Graphics.updatePlayerList = function(players, teams, moderator) {
-  // Clear the list...
-  goog.dom.removeChildren(tetriweb.Graphics.playerList_);
-  // And fill it again !
-  for (var player_num in players) {
-    var player = goog.dom.createDom('div');
-    var nickNode;
-    if (player_num == moderator) {
-      nickNode = goog.dom.createDom('strong');
-      goog.dom.setTextContent(nickNode, players[player_num]);
-    } else {
-      nickNode = goog.dom.createTextNode(players[player_num]);
-    }
-    goog.dom.appendChild(player, nickNode);
-    if (teams[player_num] != undefined && teams[player_num] != '') {
-      var teamNode = goog.dom.createTextNode(' (' + teams[player_num] + ')');
-      goog.dom.appendChild(player, teamNode);
-    }
-    goog.dom.appendChild(tetriweb.Graphics.playerList_, player);
-  }
+tetriweb.Graphics.preloadImages = function() {
+  var theme = new Image();
+  theme.src = 'img/theme.png';
 };
 
 
@@ -394,6 +387,33 @@ tetriweb.Graphics.updateGrid = function(gameArea) {
         block.style.left = c * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
         goog.dom.classes.set(block, 'block ' + convert(gameArea[l][c]));
         myField_.appendChild(block);
+      }
+    }
+  }
+};
+
+
+/**
+ * Takes the current piece element and update the blocks contained.
+ * @param {Array.<Array.<number>>} nextPiece The next piece matrix.
+ * @param {number} nextId The next piece ID.
+ */
+tetriweb.Graphics.updateNextPiece = function(nextPiece, nextId) {
+  var convert = tetriweb.Tetris.convert;
+  // That sucks.
+  var getColor = tetriweb.Tetris.getColor;
+
+  var nextPieceObj = goog.dom.getElement('next-piece');
+  goog.dom.removeChildren(nextPieceObj);
+  for (var l = 0; l < tetriweb.Tetris.DIM_PIECE_; l++) {
+    for (var c = 0; c < tetriweb.Tetris.DIM_PIECE_; c++) {
+      if (nextPiece[l][c]) {
+        var block = goog.dom.createDom('div');
+        // TODO: +3 ??
+        block.style.top = (l * tetriweb.Graphics.BLOCK_SIZE_ + 3) + 'px';
+        block.style.left = (c * tetriweb.Graphics.BLOCK_SIZE_ + 3) + 'px';
+        goog.dom.classes.set(block, 'block ' + convert(getColor(nextId)));
+        goog.dom.appendChild(nextPieceObj, block);
       }
     }
   }
@@ -435,28 +455,50 @@ tetriweb.Graphics.updatePiece = function(current, curX, curY, currentColor) {
 
 
 /**
- * Takes the current piece element and update the blocks contained.
- * @param {Array.<Array.<number>>} nextPiece The next piece matrix.
- * @param {number} nextId The next piece ID.
+ * Updates the player list.
+ * @param {Array.<string>} players The player list.
+ * @param {Array.<string>} teams The team list.
+ * @param {number} moderator The moderator's playernum.
  */
-tetriweb.Graphics.updateNextPiece = function(nextPiece, nextId) {
-  var convert = tetriweb.Tetris.convert;
-  // That sucks.
-  var getColor = tetriweb.Tetris.getColor;
-
-  var nextPieceObj = goog.dom.getElement('next-piece');
-  goog.dom.removeChildren(nextPieceObj);
-  for (var l = 0; l < tetriweb.Tetris.DIM_PIECE_; l++) {
-    for (var c = 0; c < tetriweb.Tetris.DIM_PIECE_; c++) {
-      if (nextPiece[l][c]) {
-        var block = goog.dom.createDom('div');
-        // TODO: +3 ??
-        block.style.top = (l * tetriweb.Graphics.BLOCK_SIZE_ + 3) + 'px';
-        block.style.left = (c * tetriweb.Graphics.BLOCK_SIZE_ + 3) + 'px';
-        goog.dom.classes.set(block, 'block ' + convert(getColor(nextId)));
-        goog.dom.appendChild(nextPieceObj, block);
-      }
+tetriweb.Graphics.updatePlayerList = function(players, teams, moderator) {
+  // Clear the list...
+  goog.dom.removeChildren(tetriweb.Graphics.playerList_);
+  // And fill it again !
+  for (var player_num in players) {
+    var player = goog.dom.createDom('div');
+    var nickNode;
+    if (player_num == moderator) {
+      nickNode = goog.dom.createDom('strong');
+      goog.dom.setTextContent(nickNode, players[player_num]);
+    } else {
+      nickNode = goog.dom.createTextNode(players[player_num]);
     }
+    goog.dom.appendChild(player, nickNode);
+    if (teams[player_num] != undefined && teams[player_num] != '') {
+      var teamNode = goog.dom.createTextNode(' (' + teams[player_num] + ')');
+      goog.dom.appendChild(player, teamNode);
+    }
+    goog.dom.appendChild(tetriweb.Graphics.playerList_, player);
+  }
+};
+
+
+/**
+ * Updates the special bar displayed under the game field.
+ * @param {Array.<number>} specialsQueue The specials queue to display.
+ */
+tetriweb.Graphics.updateSpecialBar = function(specialsQueue) {
+  var convert = tetriweb.Tetris.convert;
+
+  // Clear the bar...
+  goog.dom.removeChildren(tetriweb.Graphics.specialBar_);
+  // And fill it again !
+  for (var i = 0; i < specialsQueue.length; i++) {
+    var special = goog.dom.createDom('div');
+    goog.dom.classes.set(special, 'block ' + convert(specialsQueue[i]));
+    special.style.top = 0 + 'px';
+    special.style.left = i * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
+    goog.dom.appendChild(tetriweb.Graphics.specialBar_, special);
   }
 };
 
@@ -472,41 +514,13 @@ tetriweb.Graphics.setLevel = function(level) {
 
 
 /**
- * Empties the game field.
+ * Sets the error message on the login form.
+ * @param {string} message The message to set.
  */
-tetriweb.Graphics.emptyField = function() {
-  goog.dom.removeChildren(tetriweb.Graphics.myField_);
-};
-
-
-/**
- * Moves the current piece to the given horizontal position.
- * @param {number} posX Horizontal coordinate.
- */
-tetriweb.Graphics.moveCurPieceH = function(posX) {
-  tetriweb.Graphics.currentObj_.style.left =
-      posX * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
-};
-
-
-/**
- * Moves the current piece to the given vertical position.
- * @param {number} posY Vertical coordinate.
- */
-tetriweb.Graphics.moveCurPieceV = function(posY) {
-  tetriweb.Graphics.currentObj_.style.top =
-      posY * tetriweb.Graphics.BLOCK_SIZE_ + 'px';
-};
-
-
-/**
- * Hides the login form.
- */
-tetriweb.Graphics.hideLoginForm = function() {
-  var form = goog.dom.getElement('connect-form');
+tetriweb.Graphics.setErrorMessage = function(message) {
   var errorMessage = goog.dom.getElement('error-message');
-  goog.dom.classes.add(form, 'hid');
-  goog.dom.classes.add(errorMessage, 'hid');
+  goog.dom.setTextContent(errorMessage, message);
+  goog.dom.classes.remove(errorMessage, 'hid');
 };
 
 
@@ -516,19 +530,6 @@ tetriweb.Graphics.hideLoginForm = function() {
 tetriweb.Graphics.showLoginForm = function() {
   var form = goog.dom.getElement('connect-form');
   goog.dom.classes.remove(form, 'hid');
-};
-
-
-/**
- * Hides the pause and resume buttons.
- * @param {boolean} enable Whether enable or disable the buttons.
- */
-tetriweb.Graphics.enablePauseResumeButtons = function(enable) {
-  var pauseButton = goog.dom.getElement('pause-game');
-  var resumeButton = goog.dom.getElement('resume-game');
-
-  pauseButton.disabled = !enable;
-  resumeButton.disabled = !enable;
 };
 
 
